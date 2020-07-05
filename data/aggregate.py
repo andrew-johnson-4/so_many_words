419935f97808c4ebaf79121092b09e86576cf96d
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-07-04 17:06:59 -0700

    bump with major features added and a script to gather seed vocabulary from wiktionary

--- data/aggregate.py
+++ data/aggregate.py
@@ -1,3 +1,4 @@
+import time
 import os
 import re
 import random
@@ -17,19 +18,21 @@ parser.set_default_language('english')
 
 def rw_pos(v, force=False):
    if not force and v in passed: return
-   word = parser.fetch(v)
-   pos = set()
-   rel = set()
-   for w in word:
-      for d in w["definitions"]:
-         pos.add(d["partOfSpeech"])
-         for r in d["relatedWords"]:
-            if r['relationshipType'] in ['synonyms','antonyms']:
-               for wr in r["words"]:
-                  for nv in re.findall("\\w+",wr):
-                     if nv not in passed:
-                        frontier.add(nv.lower())
-   outfile.write(v+" "+",".join(pos)+"\n")
+   try:
+      word = parser.fetch(v)
+      pos = set()
+      rel = set()
+      for w in word:
+         for d in w["definitions"]:
+            pos.add(d["partOfSpeech"])
+            for r in d["relatedWords"]:
+               if r['relationshipType'] in ['synonyms','antonyms']:
+                  for wr in r["words"]:
+                     for nv in re.findall("\\w+",wr):
+                        if nv not in passed:
+                           frontier.add(nv.lower())
+      outfile.write(v+" "+",".join(pos)+"\n")
+   except: pass
    passed.add(v)
 
 if len(passed)>0:
@@ -39,3 +42,4 @@ if len(passed)>0:
 while len(frontier)>0:
    w = frontier.pop()
    rw_pos(w)
+   time.sleep(1)
