1493b2458f77c4e57f6290d335d51582b9348dc2
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-07-06 15:23:45 -0700

    generate 42000 words from the Gutenberg corpus

--- src/bin_generate_empty_dictionary.rs
+++ src/bin_generate_empty_dictionary.rs
@@ -2,8 +2,10 @@ use std::env;
 use so_many_words::util::read_lines;
 use so_many_words::en::tokens::Tokenizer;
 use std::collections::HashSet;
+use std::fs::File;
+use std::io::prelude::*;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let tokenizer = Tokenizer::new();
     let mut dict: HashSet<String> = HashSet::new();
     let args: Vec<String> = env::args().collect();
@@ -15,12 +17,18 @@ fn main() {
                 let ws = tokenizer.tokenize(&sent);
                 for w in ws.iter() {
                    if tokenizer.is_word(w) {
-                      dict.insert(w.to_string());
+                      dict.insert(w.to_lowercase());
                    }
                 }
              }
           }
        }
    }
-   println!("{}", dict.len());
+
+   let mut file = File::create("data/empty_dictionary.txt")?;
+   for w in dict.iter() {
+      file.write_all(format!("{}\tnone\n", w).as_bytes())?;
+   }
+   println!("produced {} new words", dict.len());
+   Ok(())
 }
