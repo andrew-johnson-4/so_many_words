78f723eea22b56646005fc35ce75b099a7c0f6dc
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-07-07 16:39:48 -0700

    add phonetic hashing algorithm to subcommand

---
+++ src/bin_eudex.rs
@@ -0,0 +1,23 @@
+use std::env;
+use so_many_words::util::read_lines;
+use so_many_words::en::tokens::Tokenizer;
+use eudex::Hash;
+
+fn main() {
+    let tokenizer = Tokenizer::new();
+    let args: Vec<String> = env::args().collect();
+    for fp in args[1..].iter() {
+       if fp.starts_with("--") { continue; }
+       if let Ok(lines) = read_lines(fp) {
+          for line in lines {
+             if let Ok(sent) = line {
+                let ws = tokenizer.tokenize(&sent);
+                for w in ws.iter() {
+                   let hw: u64 = Hash::new(w).into();
+                   println!("{}", hw);
+                }
+             }
+          }
+       }
+   }
+}
