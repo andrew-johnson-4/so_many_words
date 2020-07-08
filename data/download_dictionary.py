88c22396481c055c1d4074c445df13f427b03324
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-07-07 18:50:58 -0700

    separate dictionaries by determined language

--- data/download_dictionary.py
+++ data/download_dictionary.py
@@ -1,6 +1,6 @@
 """
-   The contents of the file
-       https://www.subarctic.org/manually_tagged_dictionary.txt
+   The contents of the files
+       https://www.subarctic.org/manually_tagged_dictionary_[language].txt
    are released by Andrew Johnson, (c) 2020, as an indirect part
    of this open source work, so_many_words, and under the same dual
    license structure.
@@ -11,6 +11,8 @@
 """
 
 import requests
-url = "https://www.subarctic.org/manually_tagged_dictionary.txt"
-res = requests.get(url, allow_redirects=True)
-open("data/manually_tagged_dictionary.txt", "wb").write(res.content)
+
+for lang in ["eng"]:
+   url = "https://www.subarctic.org/manually_tagged_dictionary_" + lang + ".txt"
+   res = requests.get(url, allow_redirects=True)
+   open("data/manually_tagged_dictionary_" + lang + ".txt", "wb").write(res.content)
