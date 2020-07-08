bc5515763b4540e4f041b671f791b80c635db5ab
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-07-08 14:25:06 -0700

    phoneme search is working, however eudex is sort of underwhelming

--- src/bin_search_phoneme.rs
+++ src/bin_search_phoneme.rs
@@ -1,6 +1,9 @@
+use std::env;
 use std::fs::File;
-use fst::Map;
+use fst::{Map,IntoStreamer,Streamer};
+use fst::automaton::Levenshtein;
 use memmap::Mmap;
+use eudex::Hash;
 
 fn main() {
    let lang = "eng";
@@ -10,5 +13,26 @@ fn main() {
          &File::open(&load_file).expect(&format!("No file data file: {}", load_file))
       ).expect("unable to mmap file")
    };
-   let _map = Map::new(mmap).expect("Could not create fst::Map from file");
+   let map = Map::new(mmap).expect("Could not create fst::Map from file");
+
+   let args: Vec<String> = env::args().collect();
+   for term in args[1..].iter() {
+      let th = Hash::new(term);
+      let lev = Levenshtein::new(term, 3).unwrap();
+      let mut stream = map.search(&lev).into_stream();
+
+      println!("{}", term);
+      let mut results = Vec::new();
+      while let Some((key,val)) = stream.next() {
+         let key = String::from_utf8_lossy(key).to_string();
+         let kh = Hash { hash:val };
+         let dh = (th - kh).dist();
+         results.push((dh, key));
+      }
+
+      results.sort();
+      for (_,term) in results[..10].iter() {
+         println!("\t{}", term);
+      }
+   }
 }
